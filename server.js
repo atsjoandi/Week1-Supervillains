f1900df6ff1f0a6c50152e42d0eb9aeb495db35b
atsjoandi <ats.joandi@gmail.com>
2021-09-02 17:35:18 +0300

    Supervillains

---
+++ server.js
@@ -0,0 +1,7 @@
+var supervillains = require('supervillains');
+var mySupervillainName = supervillains.random();
+console.log(mySupervillainName);
+
+var supervillains = require('supervillains');
+var mySupervillainName = supervillains.all;
+console.log(mySupervillainName);
